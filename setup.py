0533f02e238758086bd92ac7a34a47d65ae99705
aerth <aerth@riseup.net>
2018-06-28 02:22:16 -0700

    versions are flyin by

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from setuptools import setup
-version = '0.0.7'
+version = '0.0.8'
 setup(
     install_requires = ['base58', 'mnemonic', 'web3', 'ethereum'],
     name='aquachain.py',
